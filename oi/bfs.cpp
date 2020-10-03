67cfb5f88fcf16dbc40a2e3a5f6e8cf55ec78a07
Dovuq <zhent.yang@foxmail.com>
2020-10-03 18:34:28 +0800

    v18.1

--- oi/bfs.cpp
+++ oi/bfs.cpp
@@ -1,23 +1,23 @@
-// 这里使用了边表，不熟悉边表的出门右转“边表”
-// 时间复杂度 O(kn)，其中第 12 行与 20 行处理的总时间复杂度为 k
+// ����ʹ���˱߱�������Ϥ�߱��ĳ�����ת���߱���
+// ʱ�临�Ӷ� O(kn)�����е� 12 ���� 20 �д�������ʱ�临�Ӷ�Ϊ k
 enum {white,gray,black} visit[size];
 void bfs (int s)
 {
     memset (visit,white,sizeof(visit));
-    queue<int> que; // 使用了 STL queue
+    queue<int> que; // ʹ���� STL queue
     que.push (s);
     while (!que.empty())
     {
         int u=que.pop();
-        // 对该节点 u 进行一些处理，具体看题目要求
+        // �Ըýڵ� u ����һЩ���������忴��ĿҪ��
         for (int e=head[u];e;e=next[u])
         {
             int v=to[e];
-            if (visit[v]==white /*&& 题目规定的要求*/)
+            if (visit[v]==white /*&& ��Ŀ�涨��Ҫ��*/)
             {
                 que.push(v);
                 visit[v]=gray;
-                // 这里其实也可以进行一些操作
+                // ������ʵҲ���Խ���һЩ����
             }
         }
         visit[u]=black;
